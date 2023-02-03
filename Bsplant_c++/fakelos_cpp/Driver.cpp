1c0bdf117e9019eed200b4c5eb8b8711622fd4db
Marios Iosifidis <94121668+wereal@users.noreply.github.com>
2023-02-03 07:28:06 +0200

    3.2.2023
    3.2.2023

---
+++ Bsplant_c++/fakelos_cpp/Driver.cpp
@@ -0,0 +1,6 @@
+#include Driver.h
+#include Vehicle.h
+
+void Driver::drive(Vehicle vehicle, std::string destination) {
+    // implementation details
+}
---
+++ Bsplant_c++/fakelos_cpp/Employee.cpp
@@ -0,0 +1,10 @@
+#include Employee.h
+#include Vehicle.h
+
+void Employee::travel(Vehicle vehicle) {
+    // implementation details
+}
+
+void Employee::perform_task(Vehicle vehicle) {
+    // implementation details
+}
